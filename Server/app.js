54e582e5a0534a4af5d5c8e5774f5a7601b5fc0f
mingd1023 <mingd1023@gmail.com>
2020-10-27 18:18:10 +0900

    [BE] Feat: sequelize 연결
    sequelize 연결

--- Server/app.js
+++ Server/app.js
@@ -7,8 +7,18 @@ import logger from "morgan";
 import indexRouter from "./routes/index.js";
 require("dotenv").config();
 
-const app = express();
+const models = require("./models");
 
+const app = express();
+models.sequelize
+  .sync()
+  .then(() => {
+    console.log(" DB 연결 성공");
+  })
+  .catch((err) => {
+    console.log("연결 실패");
+    console.log(err);
+  });
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,24 +29,25 @@ app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
-	console.log(`App listening to ${PORT}....`);
-	console.log("Press Ctrl+C to quit.");
+  console.log(`App listening to ${PORT}....`);
+  console.log("Press Ctrl+C to quit.");
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.json({ error: "error" });
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: "error" });
+  models.sequelize.sync();
 });
 
 module.exports = app;
