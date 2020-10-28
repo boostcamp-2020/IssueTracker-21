8f0200813b531deb745a119d022b1a1d5ce71b3c
LeeGukShin <nw4611@naver.com>
2020-10-28 17:25:17 +0900

    [BE] Fix : 컨트롤러 참조 위치 오류 수정 라벨 컨트롤러 파일의 위치 참조 오류가 있어서 수정

--- Server/routes/label/index.js
+++ Server/routes/label/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const labelController = require('../controllers/labelController')
+const labelController = require('../../controllers/labelController')
 
 router.get('/', labelController.getLabels);
 router.post('/', labelController.insertLabel);
 router.put('/', labelController.updateLabel);
 router.delete('/', labelController.deleteLabel);
 
-module.export = router;
+module.exports = router;
