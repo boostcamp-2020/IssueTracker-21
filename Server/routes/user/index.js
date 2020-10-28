c1fbbf9d368c0ffe00c0649e27178690afdd34e3
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-10-28 23:33:45 +0900

    [BE] Fix : User Router 수정
    github 로그인 라우팅을 post에서 get으로 변경했습니다. 또한 라우팅되는 변수의 이름을 변경했습니다.

--- Server/routes/user/index.js
+++ Server/routes/user/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const userController = require("../../controllers/userController");
 const router = express.Router();
 
-router.post("/login", userController.login);
+router.post("/login", userController.localLogin);
 
-router.post("/github", userController.githubLogin);
-router.post("/github/callback", userController.githubCallback);
+router.get("/github", userController.githubLogin);
+router.get("/github/callback", userController.githubCallback);
 
 module.exports = router;
