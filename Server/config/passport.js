f01ceb5baf594850d1105e50cb08462324865555
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-10-28 16:32:33 +0900

    [BE] Feat: Passport JWT Strategy 설정
    Json Web Token 인증 strategy 설정

--- Server/config/passport.js
+++ Server/config/passport.js
@@ -2,7 +2,9 @@ const passport = require("passport");
 const passportJWT = require("passport-jwt");
 
 const LocalStrategy = require("passport-local").Strategy;
-const userController = require("../models/index");
+const JWTStrategy = passportJWT.Strategy;
+const userController = require("../controllers/userController"); //userDB 연동 controller
+const cookieExtractor = require("../utils/cookieExtractor"); //req.cookies 토큰 추출
 
 require("dotenv").config();
 
@@ -11,8 +13,7 @@ const LocalStrategyOption = {
   passwordField: "password",
 };
 async function localVerify(userId, password, done) {
-  // db 조회 - 결과 값 result
-  //const user = await userController.login(userId, password);
+  const user = await userController.login(userId, password);
   if (!user.success) {
     return done(null, false, {
       message: "아이디와 패스워드를 다시 확인해주세요",
@@ -21,6 +22,19 @@ async function localVerify(userId, password, done) {
   return done(null, [user], { message: "로그인에 성공했습니다." });
 }
 
+const jwtStrategyOption = {
+  jwtFromRequest: cookieExtractor,
+  secretOrKey: process.env.JWT_SECRET,
+};
+async function jwtVerift(payload, done) {
+  const result = await userController.isExist(payload.id);
+  if (!result) {
+    return done(null, false, { message: "토큰 인증에 실패했습니다." });
+  }
+  return done(null, [result]);
+}
+
 module.exports = () => {
   passport.use(new LocalStrategy(LocalStrategyOption, localVerify));
+  passport.use(new JWTStrategy(jwtStrategyOption, jwtVerift));
 };
