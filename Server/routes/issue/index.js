24f211ad4e85b8f4d4b453bc2fb02e11387e084e
mingd1023 <mingd1023@gamail.com>
2020-10-28 19:14:58 +0900

    [BE] Feat: Issue router 구현
    모든 이슈 조회, 이슈 상세 페이지 조회, 새로운 이슈 생성, 제목 / 내용 / 상태 수정, 이슈에 라벨 / 담당자 / 마일스톤 추가, 이슈 삭제 라우터 구현

---
+++ Server/routes/issue/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const issueController = require("../../controllers/issueController");
+
+/* 모든 이슈 조회 */
+router.get("/", issueController.getAllIssues);
+
+/* 이슈 상세 페이지 조회 */
+router.get("/:issueId", issueController.getIssueDetail);
+
+/* 새로운 이슈 생성 */
+router.post("/", issueController.insertNewIssue);
+
+/* 이슈 수정 - 제목 */
+router.put("/title", issueController.updateIssueTitle);
+
+/* 이슈 수정 - 내용 */
+router.put("/description", issueController.updateIssueDescription);
+
+/* 이슈 수정- status */
+router.put("/status", issueController.updateIssueStatus);
+
+/* 이슈에 라벨 추가 */
+router.post("/label", issueController.insertNewLabel);
+
+/* 이슈에 담당자 추가 */
+router.post("/assignee", issueController.insertNewAssignee);
+
+/* 이슈에 마일스톤 추가 */
+router.put("/milestone", issueController.insertNewMilestone);
+
+/* 이슈 삭제 */
+router.delete("/", issueController.deleteIssue);
+
+module.exports = router;
