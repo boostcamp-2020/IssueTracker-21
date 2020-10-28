f29a699d20bd863658dc7172d9b8a4ac94f3d992
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-10-28 17:07:07 +0900

    [BE] Feat: Passport GitHub Strategy 설정
    GitHub 인증을 위한 strategy 설정
    
    #4

--- Server/config/passport.js
+++ Server/config/passport.js
@@ -3,6 +3,7 @@ const passportJWT = require("passport-jwt");
 
 const LocalStrategy = require("passport-local").Strategy;
 const JWTStrategy = passportJWT.Strategy;
+const GitHubStrategy = require("passport-github").Strategy;
 const userController = require("../controllers/userController"); //userDB 연동 controller
 const cookieExtractor = require("../utils/cookieExtractor"); //req.cookies 토큰 추출
 
@@ -22,12 +23,37 @@ async function localVerify(userId, password, done) {
   return done(null, [user], { message: "로그인에 성공했습니다." });
 }
 
+const githubStrategyOption = {
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.GITHUB_CALLBACK_URL,
+};
+async function githubVerify(accessToken, refreshToken, profile, done) {
+  try {
+    const userId = profile.id;
+    const userName = profile.username;
+    const photoUrl = profile.photos[0].value;
+
+    const result = await userController.gitlogin(userId);
+    const user = { userId, userName, photoUrl };
+
+    // 성공
+    if (result) {
+      return done(null, user);
+    }
+    // done에 답기는 두번째 인자는 이후 사용 될 user 값
+    return done(null, false);
+  } catch (err) {
+    return done(null, false);
+  }
+}
+
 const jwtStrategyOption = {
   jwtFromRequest: cookieExtractor,
   secretOrKey: process.env.JWT_SECRET,
 };
 async function jwtVerift(payload, done) {
-  const result = await userController.isExist(payload.id);
+  const result = await userController.isExist(payload.userId);
   if (!result) {
     return done(null, false, { message: "토큰 인증에 실패했습니다." });
   }
@@ -36,5 +62,6 @@ async function jwtVerift(payload, done) {
 
 module.exports = () => {
   passport.use(new LocalStrategy(LocalStrategyOption, localVerify));
+  passport.use(new GitHubStrategy(githubStrategyOption, githubVerify));
   passport.use(new JWTStrategy(jwtStrategyOption, jwtVerift));
 };
