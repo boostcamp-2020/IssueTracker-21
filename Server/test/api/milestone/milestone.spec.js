46e920c2b1f58533d30d3cbb72c8dce4df944d95
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-10-29 17:34:16 +0900

    [TEST] Feat: 마일스톤 삽입 api 테스트 코드 작성
    마일스톤을 삽입하는 테스트 코드를 작성했습니다.

--- Server/test/api/milestone/milestone.spec.js
+++ Server/test/api/milestone/milestone.spec.js
@@ -26,3 +26,39 @@ describe("GET /api/milestone - 마일스톤 조회", () => {
       });
   });
 });
+
+/* 마일스톤 삽입 테스트*/
+describe("POST /api/milestone - 마일스톤 삽입", () => {
+  // 마일스톤 삽입
+  it("should return 200 and insert new data", (done) => {
+    request(app)
+      .post("/api/milestone")
+      .send({
+        title: "Milestone1",
+        description: "test1",
+        dueDate: "2020-10-10T00:00:00.000Z",
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      });
+  });
+
+  //삽입된 마일스톤이 존재하는지 확인
+  it("should exist data", (done) => {
+    request(app)
+      .get("/api/milestone")
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        res.body.milestones.should.matchAny(function (it) {
+          it.should.have.properties({
+            title: "Milestone1",
+            description: "test1",
+          });
+        });
+        done();
+      });
+  });
+});
