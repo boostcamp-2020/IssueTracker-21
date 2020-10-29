7091b1b519a90838a02d44b9a8855b48dc93676d
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-10-29 18:07:43 +0900

    [TEST] Feat: 마일스톤 삭제 api 테스트 코드 작성
    마일스톤을 삭제하는 테스트 코드를 작성했습니다.

--- Server/test/api/milestone/milestone.spec.js
+++ Server/test/api/milestone/milestone.spec.js
@@ -65,7 +65,7 @@ describe("POST /api/milestone - 마일스톤 삽입", () => {
 });
 
 /* 마일스톤 수정 테스트*/
-describe("POST /api/milestone - 마일스톤 삽입", () => {
+describe("PUT /api/milestone - 마일스톤 수정", () => {
   let milestone_id;
 
   //db에 있는 label id 미리 가져오기
@@ -131,6 +131,58 @@ describe("POST /api/milestone - 마일스톤 삽입", () => {
             title: "Milestone1",
             description: "test1",
             dueDate: "2020-10-10T00:00:00.000Z",
+          });
+        });
+        done();
+      });
+  });
+});
+
+/* 마일스톤 삭제 테스트*/
+describe("DELETE /api/milestone - 마일스톤 삭제", () => {
+  let milestone_id;
+
+  //db에 있는 label id 미리 가져오기
+  before(() => {
+    return new Promise((resolve) => {
+      request(app)
+        .get("/api/milestone")
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err;
+          milestone_id = res.body.milestones[res.body.milestones.length - 1].id;
+          resolve();
+        });
+    });
+  });
+
+  // 마일스톤 삭제
+  it("should return 200 and change data", (done) => {
+    request(app)
+      .delete("/api/milestone")
+      .send({
+        milestoneId: milestone_id,
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      });
+  });
+
+  //삭제 된 마일스톤이 존재하는지 확인
+  it("should not exist data", (done) => {
+    request(app)
+      .get("/api/milestone")
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        res.body.milestones.should.not.matchAny(function (it) {
+          it.should.have.properties({
+            id: milestone_id,
+            title: "Milestone2",
+            description: "test2",
+            dueDate: "2020-12-12T00:00:00.000Z",
           });
         });
         done();
