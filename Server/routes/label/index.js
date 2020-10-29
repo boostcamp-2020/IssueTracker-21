f382c24a73d247d0544e5ac4e94b90f9f34b713f
LeeGukShin <nw4611@naver.com>
2020-10-29 17:28:38 +0900

    [BE] Refactor: 라벨 라우터 주석 추가

--- Server/routes/label/index.js
+++ Server/routes/label/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 const labelController = require('../../controllers/labelController')
 
+/* 모든 라벨 가져오기 */
 router.get('/', labelController.getLabels);
+
+/* 라벨 추가 */
 router.post('/', labelController.insertLabel);
+
+/* 라벨 업데이트 */
 router.put('/', labelController.updateLabel);
+
+/* 라벨 삭제 */
 router.delete('/', labelController.deleteLabel);
 
 module.exports = router;
