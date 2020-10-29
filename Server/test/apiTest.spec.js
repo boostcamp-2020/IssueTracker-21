b16129628a74040f05462ca2463a33ca2bf9e2b4
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-10-29 15:37:28 +0900

    [TEST] Feat : apiTest.spec 작성
    모든 apiTest파일을 불러와 실행시켜주는 apiTest.spec을 작성했습니다.

---
+++ Server/test/apiTest.spec.js
@@ -0,0 +1,11 @@
+const labelSpec = require("./api/label/label.spec");
+const userSpec = require("./api/user/user.spec");
+const issueSpec = require("./api/issue/issue.spec");
+const commentSpec = require("./api/comment/comment.spec");
+const milestoneSpec = require("./api/milestone/milestone.spec");
+
+labelSpec;
+userSpec;
+issueSpec;
+commentSpec;
+milestoneSpec;
