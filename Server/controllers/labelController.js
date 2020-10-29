632c16b6f0cbc6b27537c0ae31db15f1ac47e8cd
LeeGukShin <nw4611@naver.com>
2020-10-29 17:29:55 +0900

    [BE] Refactor : labelController에서 Dao 분리

--- Server/controllers/labelController.js
+++ Server/controllers/labelController.js
@@ -1,80 +1,75 @@
-const labelModel = require('../models').label;
+const labelDao = require("../dao/labelDao");
 
+/* 모든 라벨 가져오기 */
 
 exports.getLabels = async (req, res, next) => {
-    try {
-        const labelList = await labelModel.findAll();
-        return res.status(200).json({
-            success: true,
-            labels: labelList,
-        });
-    }
-    catch (e) {
-        return res.status(400).json({
-            success: false,
-            error: e,
-        });
-    }
+  try {
+    const labelDaoResponse = await labelDao.getLabels();
+
+    return res.status(200).json({
+      success: true,
+      labels: labelDaoResponse.labelList,
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      error: e,
+    });
+  }
 };
 
+/* 라벨 추가 */
 
-exports.insertLabel = async(req, res, next) => {
-    try{
-        const {name, description, color} = req.body;
-        await labelModel.create({
-            name,
-            description,
-            color,
-        });
+exports.insertLabel = async (req, res, next) => {
+  try {
+    const { name, description, color } = req.body;
+    await labelDao.insertLabel({
+      name,
+      description,
+      color,
+    });
 
-        return res.status(200).json({
-            success: true,
-        });
-    }
-    catch(e) {
-        return res.status(400).json({
-            success: false,
-            error: e,
-        });
-    }
+    return res.status(200).json({ success: true });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      error: e,
+    });
+  }
 };
 
-exports.updateLabel = async(req, res, next) => {
-    try {
-        const {labelId, name, description, color} = req.body;
-        await labelModel.update({
-            name,
-            description,
-            color,
-        }, {where: {id : labelId}} );
+/* 라벨 업데이트 */
 
-        return res.status(200).json({
-            success: true,
-        });
-    }
-    catch(e) {
-        res.status(400).json({
-            success: false,
-            error: e,
-        });
-    }
-}
+exports.updateLabel = async (req, res, next) => {
+  try {
+    const { labelId, name, description, color } = req.body;
+    await labelDao.updateLabel({ labelId, name, description, color });
+
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: e,
+    });
+  }
+};
+
+/* 라벨 삭제 */
 
 exports.deleteLabel = async (req, res, next) => {
-    try{
-        const { labelId } = req.body;
-        await labelModel.destroy({
-            where: {id: labelId}
-        });
+  try {
+    const { labelId } = req.body;
+    await labelDao.deleteLabel({ labelId });
 
-        return res.status(200).json({
-            success: true,
-        });
-    }
-    catch(e) {
-        res.status(400).json({
-            success: false,
-            error: e,
-        });
-    }
-}
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      error: e,
+    });
+  }
+};
---
+++ Server/dao/labelDao.js
@@ -0,0 +1,68 @@
+const labelModel = require("../models").label;
+
+/* 모든 라벨 가져오기 */
+
+exports.getLabels = async () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const labelList = await labelModel.findAll();
+      resolve({ labelList });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+/* 라벨 추가 */
+
+exports.insertLabel = async ({ name, description, color }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await labelModel.create({
+        name,
+        description,
+        color,
+      });
+      return resolve();
+    } catch (e) {
+      return reject(e);
+    }
+  });
+};
+
+/* 라벨 업데이트 */
+
+exports.updateLabel = async ({ labelId, name, description, color }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await labelModel.update(
+        {
+          name,
+          description,
+          color,
+        },
+        { where: { id: labelId } }
+      );
+
+      return resolve();
+    } catch (e) {
+      return reject(e);
+    }
+  });
+};
+
+/* 라벨 삭제 */
+
+exports.deleteLabel = async ({ labelId }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await labelModel.destroy({
+        where: { id: labelId },
+      });
+
+      return resolve();
+    } catch (e) {
+      return reject(e);
+    }
+  });
+};
