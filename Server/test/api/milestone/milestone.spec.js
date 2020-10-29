03e171d26f6c9331d59b770ead6769561e9732d7
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-10-29 17:44:58 +0900

    [TEST] Feat: 마일스톤 수정 api 테스트 코드 작성
    마일스톤을 수정하는 테스트 코드를 작성했습니다.

--- Server/test/api/milestone/milestone.spec.js
+++ Server/test/api/milestone/milestone.spec.js
@@ -56,6 +56,81 @@ describe("POST /api/milestone - 마일스톤 삽입", () => {
           it.should.have.properties({
             title: "Milestone1",
             description: "test1",
+            dueDate: "2020-10-10T00:00:00.000Z",
+          });
+        });
+        done();
+      });
+  });
+});
+
+/* 마일스톤 수정 테스트*/
+describe("POST /api/milestone - 마일스톤 삽입", () => {
+  let milestone_id;
+
+  //db에 있는 label id 미리 가져오기
+  before(() => {
+    return new Promise((resolve) => {
+      request(app)
+        .get("/api/milestone")
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err;
+          milestone_id = res.body.milestones[res.body.milestones.length - 1].id;
+          resolve();
+        });
+    });
+  });
+
+  // 마일스톤 수정
+  it("should return 200 and change data", (done) => {
+    request(app)
+      .put("/api/milestone")
+      .send({
+        milestoneId: milestone_id,
+        title: "Milestone2",
+        description: "test2",
+        dueDate: "2020-12-12T00:00:00.000Z",
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      });
+  });
+
+  //수정된 마일스톤이 존재하는지 확인
+  it("should exist data", (done) => {
+    request(app)
+      .get("/api/milestone")
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        res.body.milestones.should.matchAny(function (it) {
+          it.should.have.properties({
+            id: milestone_id,
+            title: "Milestone2",
+            description: "test2",
+            dueDate: "2020-12-12T00:00:00.000Z",
+          });
+        });
+        done();
+      });
+  });
+
+  //수정 전 마일스톤이 없는지 확인
+  it("should not exist original data", (done) => {
+    request(app)
+      .get("/api/milestone")
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        res.body.milestones.should.not.matchAny(function (it) {
+          it.should.have.properties({
+            id: milestone_id,
+            title: "Milestone1",
+            description: "test1",
+            dueDate: "2020-10-10T00:00:00.000Z",
           });
         });
         done();
