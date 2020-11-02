86c456e356e70bd899f9417f0d13e82819b63227
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-02 16:32:31 +0900

    [FE] Init : React 개발 webpack, babel 설정
    React FE 개발을 위한 초기 webpack, babel, dev server를 설정 했습니다.

---
+++ Client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+
+const App = () => {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+};
+export default App;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,55 +3,56 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
-	mode: "development",
-	entry: {
-		index: "./Client/index.js",
-	},
-	output: {
-		path: path.join(__dirname, "dist"),
-		filename: "[name].js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader",
-				options: {
-					cacheDirectory: true,
-					configFile: "./Client/.babelrc",
-				},
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: "html-loader",
-					},
-				],
-			},
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.(png|svg|jpg|gif|ico)$/,
-				use: ["file-loader"],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./Client/pages/indexPage/index.html",
-			filename: "./index.html",
-		}),
-		new CleanWebpackPlugin({
-			cleanAfterEveryBuildPatterns: ["dist"],
-		}),
-	],
-	optimization: {},
-	resolve: {
-		modules: ["node_modules"],
-		extensions: [".js", ".json", ".jsx", ".css"],
-	},
+  mode: "development",
+  entry: {
+    index: "./Client/src/index.js",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          cacheDirectory: true,
+          configFile: "./Client/.babelrc",
+          presets: ["@babel/env", "@babel/preset-react"],
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ico)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./Client/public/index.html",
+    }),
+  ],
+  devServer: {
+    port: 3000,
+  },
+  optimization: {},
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".js", ".json", ".jsx", ".css"],
+  },
 };
