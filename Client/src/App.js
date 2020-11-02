35a0535a90acc089c05606b97868b5fe3d2b4ff8
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-03 03:22:26 +0900

    [FE] Fix : import 수정
    import 오류로 페이지 로딩이 되지않는 문제 수정

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import LandingPage from "./views/LandingPage/LandingPage";
 
 const App = () => {
