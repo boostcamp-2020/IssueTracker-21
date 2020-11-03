497c2e6d1e23d5aa5d1ca1c2c175b909b344ee11
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-03 15:07:31 +0900

    [FE] Feat : Custom CheckBox 구현
    상태에 따른 checkbox 표시를 위한 custom checkbox를 구현했습니다.

--- Client/src/components/IssueList/IssueList.jsx
+++ Client/src/components/IssueList/IssueList.jsx
@@ -1,29 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./IssueList.scss";
+import IssueCard from "../IssueCard";
 
 const MenuStyle = styled.div`
   color: black;
 `;
 
 function IssueList() {
+  const [ChkBox, setChkBox] = useState(0);
+  const [Issues, setIssues] = useState([]);
+
+  function chkBox(box) {
+    if (ChkBox.condition) {
+      setChkBox({
+        condition: undefined,
+      });
+    } else {
+      setChkBox({
+        condition: "checked",
+      });
+    }
+  }
+
   return (
     <div>
+      <Link to="/login">sss</Link>
       <MenuStyle id="filterArea">
         <div id="leftMenu">
-          <input type="checkbox" id="issueAllChkBox" />
+          <div
+            id="issueAllChkBox"
+            onClick={chkBox}
+            className={ChkBox.condition ? ChkBox.condition : ""}
+          />
         </div>
 
         <div id="rightMenu">
-          <div id="authorOpt">Author</div>
-          <div id="labelOpt">Label</div>
-          <div id="projectsOpt">Projects</div>
-          <div id="milestonesOpt">Milestones</div>
-          <div id="assigneeOpt">Assignee</div>
-          <div id="sortOpt">Sort</div>
+          <div className="optBtn" id="authorOpt">
+            Author ▾
+          </div>
+          <div className="optBtn" id="labelOpt">
+            Label ▾
+          </div>
+          <div className="optBtn" id="milestonesOpt">
+            Milestones ▾
+          </div>
+          <div className="optBtn" id="assigneeOpt">
+            Assignee ▾
+          </div>
         </div>
       </MenuStyle>
-      <div id="issueCardArea">ss</div>
+      <div id="issueCardArea">
+        <IssueCard
+          title="hello world"
+          isOpen="true"
+          id="8"
+          time="2020-11-01"
+          authorId="tester1"
+          milestoneTitle="milestone Back-end"
+        />
+      </div>
     </div>
   );
 }
