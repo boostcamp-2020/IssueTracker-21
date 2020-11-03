adac2685c05f55db9a5a62c7b6d8a72ce429e1db
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-03 13:18:22 +0900

    [FE] Feat : IssueCard 컴포넌트 html 구조
    IssueCard 컴포넌트의 html 구조를 잡고 scss를 이용한 style을 설정했습니다.

---
+++ Client/src/components/IssueCard/IssueCard.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Router } from "react-router-dom";
+import { Link } from "react-router-dom";
+import calcTime from "../../utils/calcTime";
+import "./issueCard.scss";
+
+function IssueCard(props) {
+  const { title, id, isOpen, milestoneTitle, time, authorId } = props;
+
+  const timeData = calcTime(time);
+
+  return (
+    <Link className="issueLink" to={id}>
+      <div className="issueCard" data-issueid={id}>
+        <div className="issueChkBox">
+          <input type="checkbox" id="issueAllChkBox" />
+        </div>
+        <div className="issueContents">
+          <div className="mainInfo">
+            <div className="icon">{isOpen ? "✅" : "❌"}</div>
+            <div className="issueTitle">{title}</div>
+          </div>
+          <div className="subInfo">
+            <div className="issueSubTitle">
+              #{id} {isOpen ? "opened" : "closed"} {timeData} by {authorId}{" "}
+            </div>
+            <div className="issueMilestone">🗓 {milestoneTitle}</div>
+          </div>
+        </div>
+        <div className="sideContents">
+          <div className="assigneeInfo">⚽⚽⚽</div>
+          <div className="commentsInfo">✉ 3</div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default IssueCard;
