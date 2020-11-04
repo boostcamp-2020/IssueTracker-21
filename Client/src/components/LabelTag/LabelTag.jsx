1162343c5ca83e46ac00184e53b74cc476ccf044
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-04 16:55:21 +0900

    [FE] Style : LabelTag 스타일 수정
    LabelTag에서 styled 컴포넌트를 사용할 때 함수 내에서 호출하는 코드를 외부로 뺐습니다.

--- Client/src/components/LabelTag/LabelTag.jsx
+++ Client/src/components/LabelTag/LabelTag.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 
+const LabelStyle = styled.div`
+  background-color: ${(props) => props.color};
+  border-radius: 20px;
+  padding: 0 15px;
+  padding-top: 4px;
+  line-height: 20px;
+  font-size: 16px;
+  font-weight: 500;
+  margin-right: 5px;
+`;
+
 function LabelTag(props) {
   const { labelName, color } = props;
 
-  const LabelStyle = styled.div`
-    background-color: ${color};
-    border-radius: 20px;
-    padding: 0 15px;
-    padding-top: 4px;
-    line-height: 20px;
-    font-size: 16px;
-    font-weight: 500;
-    margin-right: 5px;
-  `;
-
-  return <LabelStyle>{labelName}</LabelStyle>;
+  return <LabelStyle color={color}>{labelName}</LabelStyle>;
 }
 
 export default LabelTag;
