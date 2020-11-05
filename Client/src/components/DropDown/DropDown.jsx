6af1a6c96160c4481f1bddbb889d8460bd6d166a
mingd1023 <mingd1023@gamail.com>
2020-11-05 18:32:15 +0900

    [FE] Feat: 드롭다운 메뉴 구현을 위한 카드 컴포넌트들 폴더 생성
    드롭다운 메뉴 구현을 위한 카드 컴포넌트들 폴더 생성
    - 마일스톤 카드
    - 라벨 카드
    - 유저 카드(assignee, author)

---
+++ Client/src/components/DropDown/DropDown.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import { Router } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./DropDown.scss";
+
+function DropDown(props) {
+  const filter = props;
+
+  return <div></div>;
+}
+
+export default DropDown;
---
+++ Client/src/components/DropDownIssueCard/DropDownIssueCard.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import { Router } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./DropDownIssueCard.scss";
+
+function DropDownIssueCard(props) {
+  const filter = props;
+
+  return <div></div>;
+}
+
+export default DropDownIssueCard;
