b90d10cf52aa2efd176e73c12cc279ea2ba568fa
mingd1023 <mingd1023@gamail.com>
2020-11-05 17:19:35 +0900

    [FE] Refactor: 글씨 색상 함수 수정

--- Client/src/components/LabelTag/LabelTag.jsx
+++ Client/src/components/LabelTag/LabelTag.jsx
@@ -16,10 +16,13 @@ const LabelStyle = styled.div`
 `;
 
 function getFontColor(color) {
-  const R = parseInt(color.slice(1, 3), 16) <= 128 ? true : false;
-  const G = parseInt(color.slice(3, 5), 16) <= 128 ? true : false;
-  const B = parseInt(color.slice(5), 16) <= 128 ? true : false;
-  return R || G || B ? "#FFFFFF" : "#000000";
+  const R = parseInt(color.slice(1, 3), 16);
+  const G = parseInt(color.slice(3, 5), 16);
+  const B = parseInt(color.slice(5), 16);
+
+  return (0.299 * R + 0.587 * G + 0.114 * B) / 255 < 0.5
+    ? "#FFFFFF"
+    : "#000000";
 }
 
 function LabelTag(props) {
