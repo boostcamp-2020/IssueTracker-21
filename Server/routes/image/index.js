0fa4ea677cb1d7e5294460723eebe692e70f14e8
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-07 05:19:22 +0900

    [BE] Refactor : image 라우터, 컨트롤러 삭제
    image를 위한 라우터와 컨트롤러를 서버에서 사용하지 않기때문에 제거했습니다.

--- Server/routes/image/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const imageController = require("../../controllers/imageController");
-const router = express.Router();
-
-/* url 라우팅 */
-router.post("/upload", imageController.uploadImage);
-
-module.exports = router;
