68e2c1770dd5701656d88d337295096ce7035c46
LeeGukShin <nw4611@naver.com>
2020-11-06 23:22:03 +0900

    [FE] Refactor&Feat : issue데이터 위치 리팩토링 & Navbar Input 엔터 핸들러 구현 issue 데이터들을 LandingPage에서 관리하도록 변수를 생성하고 Context에 등록 Navbar의 input에서 엔터 이벤트가 동작하면 issue 데이터들을 조건에 맞춰서 가져오도록 변경

--- Client/src/views/LandingPage/LandingPage.jsx
+++ Client/src/views/LandingPage/LandingPage.jsx
@@ -2,24 +2,55 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import IssueList from "../../components/IssueList";
 import Navbar from "Components/Navbar";
+import inputDataToUrl from "../../utils/searchRegex";
+import axios from "axios";
 import "./LandingPageStyle.scss";
 
 export const LandingPageContext = React.createContext();
 
 function LandingPage() {
+  //input에 입력되는 데이터를 관리
   const [inputData, setInputData] = useState("");
+  //이슈 리스트에 표시될 이슈 데이터를 관리
+  const [Issues, setIssues] = useState([]);
 
+  const issueHandler = (issueList) => {
+    setIssues(issueList);
+  };
+
+  //input에 데이터가 입력될 때 동작
   const inputOnChangeHandler = (e) => {
     setInputData(e.target.value);
   };
 
+  //input에서 엔터키가 눌렸을 때 작동법
+  const inputSubmitHandler = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      axios.get(inputDataToUrl(inputData)).then((response) => {
+        if (response.data.success) {
+          issueHandler(response.data.issues);
+        } else {
+          alert("Failed to get issues");
+        }
+      });
+    }
+  };
+
+  //필터 버튼의 리스트가 눌렸을 때 동작
   const inputOnClickHandler = (e) => {
     setInputData(e.target.id);
   };
-
   return (
     <LandingPageContext.Provider
-      value={{ inputOnChangeHandler, inputOnClickHandler, inputData }}
+      value={{
+        inputData,
+        Issues,
+        issueHandler,
+        inputOnChangeHandler,
+        inputOnClickHandler,
+        inputSubmitHandler,
+      }}
     >
       <div id="landingArea">
         <br />
@@ -27,7 +58,7 @@ function LandingPage() {
         <br />
         <Navbar />
         <br />
-        <IssueList inputData={inputData} />
+        <IssueList />
         <br />
 
         <br />
