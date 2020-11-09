9564c29820eb260237e4fde3729702f6f0c4b742
mingd1023 <mingd1023@gmail.com>
2020-11-09 17:55:56 +0900

    [FE] Feat: Created Nav Bar for Label Page

---
+++ Client/src/components/LabelNavBar/LabelNavBar.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState, useMemo } from "react";
+import CustomBtn from "../CustomBtn";
+import { Icon, InlineIcon } from "@iconify/react";
+import tagIcon from "@iconify/icons-octicon/tag";
+import milestone24 from "@iconify/icons-octicon/milestone-24";
+import CustomDropDown from "../CustomDropDown";
+import CustomInput from "../CustomInput";
+
+import "./style.css";
+
+function LabelNavBar() {
+  // function moveNewIssue() {
+  //   props.history.push("/issue/new");
+  // }
+
+  return (
+<div>hahahah</div>
+  );
+}
+
+export default LabelNavBar;
+
