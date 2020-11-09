28520942d89cc2d6fd1cd46f7b98302f5d556ee2
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-09 18:02:24 +0900

    [FE] Feat : new issuepage 유저 정보 출력
    new issue 페이지에 현재 로그인한 유저의 이미지를 출력 할 수 있도록 했습니다.

--- Client/src/views/NewIssuePage/NewIssuePage.jsx
+++ Client/src/views/NewIssuePage/NewIssuePage.jsx
@@ -1,8 +1,50 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Editor from "../../components/Editor";
+import axios from "axios";
+import noprofile from "../../../public/img/noprofile.png";
+import "./style.scss";
 
 function NewIssuePage() {
-  return <Editor />;
+  const [User, setUser] = useState(null);
+  const [Title, setTitle] = useState("");
+
+  const titleHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  useEffect(() => {
+    axios.get("/api/user/userinfo").then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+        setUser(response.data);
+      } else {
+        alert("Failed to get assignees");
+      }
+    });
+  }, []);
+  return (
+    <div id="newIssueArea">
+      <div id="profileArea">
+        <img
+          src={User ? User.user.profile : noprofile}
+          alt="profile"
+          id="issueProfile"
+        />
+      </div>
+      <div id="editorArea">
+        <input
+          id="newIssueTitle"
+          type="text"
+          placeholder="Title"
+          onChange={titleHandler}
+          value={Title}
+        />
+        <Editor />
+      </div>
+      <div id="sideBar">sidebar section</div>
+    </div>
+  );
 }
 
 export default NewIssuePage;
