27571ac11a3d8169ddeae8d9be8f70aae64379ac
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-10 04:09:38 +0900

    Refactor : imgur Util 수정
    imgur 유틸을 비동기작도을 위해 promise로 바꿔주었습니다.

--- Client/src/utils/uploadImgur.js
+++ Client/src/utils/uploadImgur.js
@@ -1,34 +1,37 @@
 import axios from "axios";
 
 function NewImage(e, Contents, setContents) {
-  try {
-    const reader = new FileReader();
+  return new Promise((resolve, reject) => {
+    try {
+      const reader = new FileReader();
 
-    reader.onloadend = () => {
-      const base64 = reader.result;
-      if (base64) {
-        const img = base64.split(",")[1].toString();
-        axios
-          .post("https://api.imgur.com/3/image", img, {
-            headers: {
-              Authorization: "Client-ID 3428d1d51b9b86e",
-              Accept: "application/json",
-            },
-          })
-          .then((response) => {
-            let imgTag = `
-
-[${response.data.data.id}](${response.data.data.link})
-
-`;
-            setContents(Contents + imgTag);
-          });
-      }
-    };
-    reader.readAsDataURL(e.target.files[0]);
-  } catch (e) {
-    return alert("이미지 업로드에 실패했습니다.");
-  }
+      reader.onloadend = () => {
+        const base64 = reader.result;
+        if (base64) {
+          const img = base64.split(",")[1].toString();
+          axios
+            .post("https://api.imgur.com/3/image", img, {
+              headers: {
+                Authorization: "Client-ID 3428d1d51b9b86e",
+                Accept: "application/json",
+              },
+            })
+            .then((response) => {
+              let imgTag = `
+  
+  [${response.data.data.id}](${response.data.data.link})
+  
+  `;
+              setContents(Contents + imgTag);
+              resolve(response.data.data);
+            });
+        }
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    } catch (e) {
+      reject("이미지 업로드에 실패했습니다.");
+    }
+  });
 }
 
 export default NewImage;
