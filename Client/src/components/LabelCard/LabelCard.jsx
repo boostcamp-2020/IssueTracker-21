dea685363c119e5556c64c56b5b45fae6a6100d2
mingd1023 <mingd1023@gmail.com>
2020-11-09 15:16:14 +0900

    [FE] Feat: implemented deletion of label

--- Client/src/components/LabelCard/LabelCard.jsx
+++ Client/src/components/LabelCard/LabelCard.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Router } from "react-router-dom";
 import { Link } from "react-router-dom";
 import LabelTag from "../LabelTag";
 import "./LabelCard.scss";
 
 function LabelCard(props) {
-  const { id, name, description, color } = props;
+  const { id, name, description, color, deleteFunction } = props;
+  //console.log(props);
+
 
   return (
     <div className="labelCard" data-labelid={id}>
       <div className="labelTagDisplay">
-        {<LabelTag key={id} labelName={name} color={color} />}
+        <LabelTag key={id} labelName={name} color={color} />
       </div>
       <div className="labelDescription">{description}</div>
       <div className="labelButtonContainer">
         <div className="labelEditButton">Edit</div>
-        <div className="labelDeleteButton">Delete</div>
+        <div className="labelDeleteButton" onClick={() => deleteFunction(id)}>Delete</div>
       </div>
     </div>
   );
