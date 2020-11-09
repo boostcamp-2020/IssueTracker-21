75465aae43e1a3d08952b00124e617e700043f27
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-10 03:27:06 +0900

    [FE] Style: register form style
    회원가입 모달의 스타일 설정

--- Client/src/components/RegisterModal/RegisterModal.jsx
+++ Client/src/components/RegisterModal/RegisterModal.jsx
@@ -1,28 +1,78 @@
 import React from "react";
+import styled from "styled-components";
 
 function RegisterModal() {
   return (
-    <div id="registerModal">
-      <div id="registerTitle">회원가입</div>
+    <RegisterModalStyle id="registerModal">
+      <RegisterTitleStyle id="registerTitle">회원가입</RegisterTitleStyle>
       <form>
-        <label htmlFor="idForm">Id</label>
-        <input type="text" id="idForm" />
+        <RegisterLabelStyle htmlFor="idForm">Id</RegisterLabelStyle>
+        <RegisterInputStyle type="text" id="idForm" />
 
-        <label htmlFor="pwFrom">password</label>
-        <input type="text" id="pwFrom" />
+        <RegisterLabelStyle htmlFor="pwFrom">password</RegisterLabelStyle>
+        <RegisterInputStyle type="password" id="pwFrom" />
 
-        <label htmlFor="pwConfirmForm">password confirm</label>
-        <input type="text" id="pwConfirmForm" />
+        <RegisterLabelStyle htmlFor="pwConfirmForm">
+          password confirm
+        </RegisterLabelStyle>
+        <RegisterInputStyle type="password" id="pwConfirmForm" />
 
-        <label htmlFor="fileForm">프로필 첨부</label>
+        <RegisterLabelStyle htmlFor="fileForm">프로필 첨부</RegisterLabelStyle>
         <input
           type="file"
           accept="image/jpeg, image/jpg, image/png"
           id="fileForm"
         />
+        <RegisterSubmitStyle>
+          <button type="submit">회원가입</button>
+        </RegisterSubmitStyle>
       </form>
-    </div>
+    </RegisterModalStyle>
   );
 }
 
 export default RegisterModal;
+
+const RegisterModalStyle = styled.div`
+  width: 50%;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const RegisterTitleStyle = styled.div`
+  width: 100%;
+  font-size: 23px;
+  font-weight: 600;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const RegisterLabelStyle = styled.label`
+  font-size: 15px;
+  font-weight: 400;
+  margin-top: 15px;
+`;
+
+const RegisterInputStyle = styled.input`
+  font-size: 20px;
+  height: 30px;
+  font-weight: 400;
+`;
+
+const RegisterSubmitStyle = styled.div`
+  width: 100%;
+  font-size: 20px;
+  margin: 20px 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const submitBtnStyle = styled.button`
+  border-radius: 6px;
+  background-color: green;
+`;
