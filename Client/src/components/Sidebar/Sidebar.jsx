733d0bf55f1d8e6dbca1ff7a38c50f94e894a0e5
LeeGukShin <nw4611@naver.com>
2020-11-10 21:53:24 +0900

    [FE] Feat: labelItem과 MilestoneItem추가

--- Client/src/components/Sidebar/Sidebar.jsx
+++ Client/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import AsigneeSideItem from "../AsigneeSideItem";
-
-
+import LabelSideItem from "../LabelSideItem";
+import MilestoneSideItem from "../MilestoneSideItem";
 
 function Sidebar() {
   return (
     <div>
       <AsigneeSideItem />
+      <hr />
+      <LabelSideItem />
+      <hr />
+      <MilestoneSideItem />
     </div>
   );
 }
