69d2c1db8eba28190d606bd7774d4f4932f8152a
LeeGukShin <nw4611@naver.com>
2020-11-10 15:45:34 +0900

    [FE] Feat: Sidebar 구현 이슈 생성화면과 수정 화면에서 추가될 Sidebar구현

---
+++ Client/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SideItem from "../SideItem";
+
+
+
+function Sidebar() {
+  return (
+    <div>
+      <SideItem />
+    </div>
+  );
+}
+
+export default Sidebar;
