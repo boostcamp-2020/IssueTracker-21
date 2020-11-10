cd88a2e12e4af81fe3d62239dfa3f401a074ecc3
LeeGukShin <nw4611@naver.com>
2020-11-10 23:17:11 +0900

    [FE] Refactor: progressbar와 텍스트 위치 변경

--- Client/src/components/MilestoneSideCard/MilestoneSideCard.jsx
+++ Client/src/components/MilestoneSideCard/MilestoneSideCard.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import Progressbar from "../Progressbar";
+import { NewIssuePageContext } from "../../views/NewIssuePage";
 
 function MilestoneSideCard({ title }) {
+  const { progress } = useContext(NewIssuePageContext);
+
   return (
     <div>
+      <Progressbar completed={progress} />
       <span>{title}</span>
-      <Progressbar completed={50} />
     </div>
   );
 }
