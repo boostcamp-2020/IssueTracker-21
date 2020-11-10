44582476a815fd00d5fa2432ab171342de9c3192
LeeGukShin <nw4611@naver.com>
2020-11-10 15:42:44 +0900

    [FE] Feat & Refactor : 간단한 리팩토링 및 Context API 추가 카드가  선택 되었을 때의 동작을 지정하기 위한 핸들러를 등록

--- Client/src/components/DropDown/DropDown.jsx
+++ Client/src/components/DropDown/DropDown.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Router } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./DropDown.scss";
@@ -7,32 +7,46 @@ import DropDownAssigneeList from "../DropDownAssigneeList";
 import DropDownAuthorList from "../DropDownAuthorList";
 import DropDownMilestoneList from "../DropDownMilestoneList";
 
-function DropDown(props) {
-  const filter = props.filter;
+export const DropDownContext = React.createContext();
 
+function DropDown(props) {
+  const dropDownRef = useRef();
+  const { onCardClicked, status, handler, filter } = props;
   const [Items, setItems] = useState([]);
-  const [SelectedItems, setSelectedItems] = useState([]);
+
+  const handleClickOutside = ({ target }) => {
+    if (status && !dropDownRef.current.contains(target)) handler(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", handleClickOutside);
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   const getProperList = (filter) => {
-    switch (filter){
+    switch (filter) {
       case "assignee":
         return <DropDownAssigneeList></DropDownAssigneeList>;
       case "author":
         return <DropDownAuthorList></DropDownAuthorList>;
       case "label":
-        return <DropDownLabelList></DropDownLabelList>;       
+        return <DropDownLabelList></DropDownLabelList>;
       case "milestone":
         return <DropDownMilestoneList></DropDownMilestoneList>;
     }
-  }
+  };
 
   return (
-  <div className="dropDownContainter">
-    <div className="dropDownBarContainter">Filter by {filter}</div>
-    <div className="dropDownListContainter">
-      {getProperList(filter)}
-    </div>
-  </div>
+    <DropDownContext.Provider value={{ onCardClicked }}>
+      {status && (
+        <div className="dropDownContainter" ref={dropDownRef}>
+          <div className="dropDownBarContainter">Filter by {filter}</div>
+          <div className="dropDownListContainter">{getProperList(filter)}</div>
+        </div>
+      )}
+    </DropDownContext.Provider>
   );
 }
 
