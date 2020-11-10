0d28584c3e9acc3af622097b716bf1e4cb5cb049
mingd1023 <mingd1023@gmail.com>
2020-11-10 19:20:03 +0900

    [FE] Refactor: css-in-js로 수정 & 헤더에 메인 링크 걺

--- Client/src/components/Header/Header.jsx
+++ Client/src/components/Header/Header.jsx
@@ -1,20 +1,53 @@
 import React from "react";
-import "./style.scss";
 import styled from "styled-components";
+
+function Header() {
+  return (
+    <HeaderDiv id="headerArea">
+      <Top>
+        <Imogi href='/'>&#128008; &nbsp;</Imogi>
+        <HeaderTitleLink href='/'>ISSUES</HeaderTitleLink>
+      </Top>
+    </HeaderDiv>
+  );
+}
 
 const HeaderDiv = styled.div`
   background-color: black;
 `;
 
-function Header() {
-  return (
-    <HeaderDiv id="headerArea">
-      <div className="top">
-        <div className="imogi">&#128008; &nbsp;</div>
-        <div className="headerTitle">ISSUES</div>
-      </div>
-    </HeaderDiv>
-  );
-}
+const Top = styled.div`
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: space-around;
+  background-color: black;
+  color: white;
+  font-size: 9px;
+  padding: 0.5%;
+  transition: 0.4s;
+  :hover {
+    opacity: 0.7;
+    transition: 0.4s;
+  }
+`;
+
+const HeaderTitleLink = styled.a`
+  color: white;
+  :hover {
+    text-decoration:none;
+    color: white;
+  }
+`;
+
+const Imogi = styled.a`
+  font-size: 15px;
+  margin: 0;
+  :hover {
+    text-decoration:none;
+    color: white;
+  }
+`;
 
 export default Header;
