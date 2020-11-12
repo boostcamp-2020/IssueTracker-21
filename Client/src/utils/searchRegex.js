d761ddabf61ceb82508ca893792e77b6a4e8feaa
LeeGukShin <nw4611@naver.com>
2020-11-12 18:04:34 +0900

    [Fe] Fix: 마일스톤, 라벨 검색 띄어쓰기 추가

--- Client/src/utils/searchRegex.js
+++ Client/src/utils/searchRegex.js
@@ -1,21 +1,19 @@
 const inputDataToUrl = (inputData) => {
   if (!inputData.length) return "api/issue";
-  const searchRegex = /(is\:(open|closed))|(milestone\:([^\s]*))|(label\:([^\s]*))|(author\:([^\s]*))|(commentor\:([^\s]*)|(assignee\:([^\s]*)))/g;
+  const searchRegex = /is\:(open|closed)|(milestone\:"([^"]*)")|(milestone\:([^\s]*))|(label\:"([^"]*)")|(label\:([^\s]*))|(author\:([^\s]*))|(commentor\:([^\s]*)|(assignee\:([^\s]*)))/g;
   const parsedData = inputData.match(searchRegex);
   if (!parsedData) return;
+
   return (
     "api/issue?" +
     parsedData
       .reduce((acc, e) => {
-        return (
-          acc +
-          e
-            .replace("is", "isOpened")
-            .replace(":", "=")
-            .replace("closed", 0)
-            .replace("open", 1) +
-          "&"
-        );
+        if (e === "is:closed") return acc + "isOpened=0&";
+        if (e === "is:open") return acc + "isOpened=1&";
+        const seperatedStr = e.split(":");
+        const tag = seperatedStr[0];
+        const completedStr = tag + "=" + seperatedStr.slice(1).join(":");
+        return acc + completedStr.replace(/"/g, "").replace(/\s/g, "+") + "&";
       }, "")
       .slice(0, -1)
   );
