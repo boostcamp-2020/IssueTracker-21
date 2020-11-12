822cd23c90fce225e9a8ea4be96715df51cb3d51
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-12 10:54:16 +0900

    [FE] Style: mark as 드롭다운 style 변경
    mark as 드롭다운 우측 공백이 생겨, 해당 공백 제거

--- Client/src/components/DropDownStatus/DropDownStatus.jsx
+++ Client/src/components/DropDownStatus/DropDownStatus.jsx
@@ -6,118 +6,117 @@ import axios from "axios";
 import { LandingPageContext } from "../../views/LandingPage";
 
 function DropDownStatus(props) {
-    const { Issues, issueHandler } = useContext(LandingPageContext);
-    const issueIdArr = props.issueIdArr;
+  const { Issues, issueHandler } = useContext(LandingPageContext);
+  const issueIdArr = props.issueIdArr;
 
-    const renderIssues = () => {
-        axios.get("/api/issue").then((response) => {
-            if (response.data.success) {
-              issueHandler(response.data.issues);
-            } else {
-              alert("Failed to get issues");
-            }
-          });
-    }
-
-    const onMaskClick = (e) => {
-        if (e.target === e.currentTarget) {
-            props.onClose(e)
-        }
+  const renderIssues = () => {
+    axios.get("/api/issue").then((response) => {
+      if (response.data.success) {
+        issueHandler(response.data.issues);
+      } else {
+        alert("Failed to get issues");
       }
-    
-      const close = (e) => {
-        if (props.onClose) {
-            props.onClose(e)
-        }
-      }
+    });
+  };
 
-    const openIssueHandler = (e) => {
-        issueIdArr.forEach((id) => {
-            axios.put("/api/issue/status", {issueId: id, newStatus: true}).then((response) => {
-                if (response.data.success) {
-                    renderIssues();
-                } else {
-                  alert("Failed to update status of issues");
-                }
-              });
-        });
-        close(e);
+  const onMaskClick = (e) => {
+    if (e.target === e.currentTarget) {
+      props.onClose(e);
     }
+  };
 
-    const closeIssueHandler = (e) => {
-        issueIdArr.forEach((id) => {
-            axios.put("/api/issue/status", {issueId: id, newStatus: false}).then((response) => {
-                if (response.data.success) {
-                    renderIssues();
-                } else {
-                  alert("Failed to update status of issues");
-                }
-              });
-        });
-        close(e);
+  const close = (e) => {
+    if (props.onClose) {
+      props.onClose(e);
     }
+  };
+
+  const openIssueHandler = (e) => {
+    issueIdArr.forEach((id) => {
+      axios
+        .put("/api/issue/status", { issueId: id, newStatus: true })
+        .then((response) => {
+          if (response.data.success) {
+            renderIssues();
+          } else {
+            alert("Failed to update status of issues");
+          }
+        });
+    });
+    close(e);
+  };
+
+  const closeIssueHandler = (e) => {
+    issueIdArr.forEach((id) => {
+      axios
+        .put("/api/issue/status", { issueId: id, newStatus: false })
+        .then((response) => {
+          if (response.data.success) {
+            renderIssues();
+          } else {
+            alert("Failed to update status of issues");
+          }
+        });
+    });
+    close(e);
+  };
 
   return (
-        <DropDownContainter visible={props.visible}>
-          <DropDownBarContainter>Actions</DropDownBarContainter>
-          <DropDownListContainter>
-            <div className="dropDownList">
-                <div className="dropDownCardContainter">
-                    <DropDownStatusCard
-                    onClick={openIssueHandler}
-                    >
-                        <div className="dropDownMilestoneName">Open</div>
-                    </DropDownStatusCard>
-                    <DropDownStatusCard
-                    onClick={closeIssueHandler}
-                    >
-                        <div className="dropDownMilestoneName">Closed</div>
-                    </DropDownStatusCard>
-                </div>
-            </div>
-          </DropDownListContainter>
-        </DropDownContainter>
-);
+    <DropDownContainter visible={props.visible}>
+      <DropDownBarContainter>Actions</DropDownBarContainter>
+      <DropDownListContainter>
+        <div className="dropDownList">
+          <div className="dropDownCardContainter">
+            <DropDownStatusCard onClick={openIssueHandler}>
+              <div className="dropDownMilestoneName">Open</div>
+            </DropDownStatusCard>
+            <DropDownStatusCard onClick={closeIssueHandler}>
+              <div className="dropDownMilestoneName">Closed</div>
+            </DropDownStatusCard>
+          </div>
+        </div>
+      </DropDownListContainter>
+    </DropDownContainter>
+  );
 }
 
 const DropDownStatusCard = styled.div`
-border-top: 1px solid rgb(225, 228, 232);
-background-color: #ffffff;
-padding: 7px;
-display: flex;
-align-items: center;
-justify-content: center;
-:hover {
+  border-top: 1px solid rgb(225, 228, 232);
+  background-color: #ffffff;
+  padding: 7px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  :hover {
     background-color: #f6f8fa;
   }
-`
+`;
 
 let DropDownContainter = styled.div`
-display: ${(props) => (props.visible ? 'block' : 'none')};
-position:absolute;
-top: 250px;
-width: 200px;
-border-radius: 6px;
-border: 1px solid rgb(225, 228, 232);
-font-size: 13px;
-display: flex;
-flex-flow: column;
-box-shadow: 0px 0px 5px rgb(225, 228, 232);
-z-index: 3;
+  display: ${(props) => (props.visible ? "block" : "none")};
+  position: absolute;
+  top: 250px;
+  width: 200px;
+  border-radius: 6px;
+  border: 1px solid rgb(225, 228, 232);
+  font-size: 13px;
+  display: flex;
+  flex-flow: column;
+  box-shadow: 0px 0px 5px rgb(225, 228, 232);
+  z-index: 3;
 `;
 
 const DropDownBarContainter = styled.div`
-border-radius: 6px 6px 0 0;
-background-color: #fafbfc;
-padding: 7px;
-font-weight: 700;
+  border-radius: 6px 6px 0 0;
+  background-color: #fafbfc;
+  padding: 7px;
+  font-weight: 700;
 `;
 
 const DropDownListContainter = styled.div`
-cursor: pointer;
-border-radius: 0 0 6px 6px;
-max-height: 200px;
-overflow: scroll;
+  cursor: pointer;
+  border-radius: 0 0 6px 6px;
+  max-height: 200px;
 `;
 
 export default DropDownStatus;
